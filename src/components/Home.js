7569c1ac132abfb584b62b085fe06de48d947566
Josefina <jstaudenmaier@udesa.edu.ar>
2021-10-18 18:19:38 -0300

    activity indicator en home¡

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Text, Image, View, Stylesheet} from 'react-native';
+import {Text, Image, View, Stylesheet, ActivityIndicator} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import Tarjeta from '../components/Tarjeta';
 
 export default function Home () {
 
-    constructor(props) { 
+    constructor(props){ 
         super(props);
         this.state ={
             movies: [],
@@ -44,9 +45,10 @@ export default function Home () {
                     
                      {this.state.movies === [] ? //si array de Movies vacío
     
-                    <Text>Cargando...</Text> 
+                    <ActivityIndicator size='large' color='blue'/>
                     : //sino
-                  this.state.filterBuscador.map ( (movies, index) => { //devolver la info de la
+                   //habria que poner un flatlist aca
+                   this.state.filterBuscador.map ( (movies, index) => { //devolver la info de la
                       return <Tarjeta className="card-movies" key = {index} index = {index}id={movies.id} title= {movies.title} rating = {movies.vote_average} image = {movies.poster_path} descripcion= {movies.overview} adult={movies.adult} release_date={movies.release_date} deleteCard={this.deleteCard} moreInfo={this.moreInfo} swap={this.swapPositions}/> 
                   })}
                 
