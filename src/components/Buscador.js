ffdd1f158ca8ad30a5a754ffc1570f5bf62eabe6
Josefina <jstaudenmaier@udesa.edu.ar>
2021-10-19 10:49:15 -0300

    Componente buscador

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -1,2 +1,32 @@
 import React from 'react';
 import {Text, Image, View, Stylesheet} from 'react-native';
+import React, { Component } from 'react';
+import { TextInput } from 'react-native-gesture-handler';
+
+
+export default class Buscador extends Component () {
+
+    constructor(props){ 
+        super(props);
+    
+        }
+
+   render(){
+    return(
+
+        <View>
+
+        <TextInput
+        style={styles.buscador}
+        keyboardType= 'default'
+        placeholder='¿Qué estás buscando?'
+       /* onChangeText= */
+        
+        />
+
+        </View>
+
+    )
+   } 
+
+}
