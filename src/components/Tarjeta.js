457ea7e98df84350704a8f443f33e308d82288e2
Josefina <jstaudenmaier@udesa.edu.ar>
2021-10-19 10:14:49 -0300

    error en tarjeta arreglado

--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Text, Image, View, Stylesheet, TouchableOpacity} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import React, { Component } from 'react';
 
-export default function Tarjeta () {
+
+export default class Tarjeta extends Component () {
     constructor(props){
         super(props)
     }
