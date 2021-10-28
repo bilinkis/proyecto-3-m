0383c1701b13d79b6ca2d56d9f28c37417c1500c
Josefina <jstaudenmaier@udesa.edu.ar>
2021-10-28 09:54:04 -0300

    boton en Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export default function Post ({item}){
    
 return(
     <View>
         <Text>{item.data.description}</Text>
+        <TouchableOpacity onPress={() => this.props.handleLikes(likes)}><Text><i class="far fa-heart"></i></Text></TouchableOpacity>
         <Text>{item.data.createdAt}</Text>
         <Text>{item.data.owner}</Text>
     </View>
