037ae6430d57bde216f1797e02ad75215bcdafca
Josefina <jstaudenmaier@udesa.edu.ar>
2021-10-28 09:53:07 -0300

    posts en home

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
 import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+import { auth } from '../firebase/config';
 
 
 export default class Home extends Component {
     constructor(props){
             super(props);
             this.state = {
-                
+                posts=[]
             }
         }
+componentDidMount(){
+    db.collection('posts').orderBy('createdAt', 'desc').onSnapshot( //onSnapshot actualiza los datos
+        docs => {
+            let postsAux = [] //Variable auxiliar
+            docs.forEach( doc => { //se recorre el array
+                postsAux.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            })
+            this.setState({
+                posts: postsAux
+            })
+        }
 
-
+    )}
+    render(){
+        return(
+            <View style = {styles.container}>
+                <Text> Home </Text>
+                <FlatList
+                data = {this.state.posts}
+                keyExtractor = {post => post.id.toString()} //pasamos el id a String
+                renderItem = { ({item}) => 
+                    <Post item = {item}></Post> }
+                />
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
