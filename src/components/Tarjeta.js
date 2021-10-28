aebc393cc98a4144d0e8cd3b4c8db1ce9bebabd8
Josefina <jstaudenmaier@udesa.edu.ar>
2021-10-28 08:30:01 -0300

    Login

--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -11,19 +11,9 @@ export default class Tarjeta extends Component () {
 
 render (){ 
         return (
-            <View>
-                                            <Image src={{uri: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2" + this.props.image }} style={styles.image-tarjeta} resizeMode='contain'/>
-                    <Text>{this.props.title}</Text>
-                    <Text>{this.props.descripcion}</Text>
-                    <TouchableOpacity onClick={() => this.props.moreInfo(this.props.id)}><Text>Ver más</Text></TouchableOpacity>                    <section className="aditional-info" id={this.props.id}>
-                       <Text>Rating: {this.props.rating}</Text>
-                        <Text>Fecha de lanzamiento: {this.props.release_date}</Text>
-                        {this.props.adult === false ?
-                        <Text>Apta para menores: Si</Text>
-                        :
-                        <Text>Apta para menores: No</Text>
-}
-            </View>
+        
+
+         
         )}
 
 
--- src/screens/login.js
+++ src/screens/login.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
-import Home from '../components/Home';
+import { TextInput } from 'react-native-gesture-handler';
+import { auth } from '../firebase/config';
 
 
-export default function Login(){
+export default class Login extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+        }
+}
+}
+
+    render(){
+    return(
+        <View>
+            <Text>
+                <TextInput
+                style={styles.input}
+                keyboardType="email-address"
+                placeholder="Email"
+                onChangeText={text=>this.setState({email:text})}              
+                />
+                <TextInput
+                style={styles.input}
+                keyboardType="default"
+                placeholder="Password (al menos 6 caracteres!)"
+                secureTextEntry={true}
+                onChangeText={text=>this.setState({password:text})}              
+                />
+            </Text>
+            <TouchableOpacity style = {styles.button} onPress={() => this.props.handleLogin(this.state.email, this.state.password)}>
+                    <Text style = {styles.text}> Login </Text>
+            </TouchableOpacity>
 
 
+
+        </View>        
+    )
 }
 
 const styles = StyleSheet.create({
