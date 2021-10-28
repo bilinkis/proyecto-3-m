06795585bbf68021c08ad5ce0ff327cfabd24ee7
Josefina <jstaudenmaier@udesa.edu.ar>
2021-10-28 11:35:43 -0300

    app js con drawer

--- App.js
+++ App.js
@@ -1,30 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Drawer from '../proyecto-3-m/src/screens/Drawer';
-import Buscador from '../proyecto-3-m/src/components/Buscador';
-import Footer from '../proyecto-3-m/src/components/Footer';
-import Home from './src/screens/Home';
-import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
-    <View style={styles.header}>
-      <NavigationContainer>
-        <Drawer/>
-      </NavigationContainer>
-      <Buscador/>
-      <Home/>
-      <Footer/>
-    </View>
+    <Drawer/>
   );
 }
-
-const styles = StyleSheet.create({
-  header: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
