eaf71d20138544ab6a509a7daabe31c1c1814496
Josefina <jstaudenmaier@udesa.edu.ar>
2021-10-29 12:13:37 -0300

    imprimimos posts en Home

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {View, Text, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
 import { auth } from '../firebase/config';
+import Post from '../components/Post';
+import { db } from '../firebase/config';
 
 
 export default class Home extends Component {
@@ -10,29 +12,36 @@ export default class Home extends Component {
                 posts:[],
             }
         }
-componentDidMount(){
-    db.collection('posts').orderBy('createdAt', 'desc').onSnapshot( //onSnapshot actualiza los datos
-        docs => {
-            let postsAux = [] //Variable auxiliar
-            docs.forEach( doc => { //se recorre el array
-                postsAux.push({
-                    id: doc.id,
-                    data: doc.data()
-                })
-            })
-            this.setState({
-                posts: postsAux
-            })
+        componentDidMount(){
+            db.collection('posts').orderBy("createdAt", "desc").onSnapshot(
+                docs => {
+                    let postsAux = []
+                    docs.forEach( doc => {
+                        postsAux.push({
+                            id: doc.id,
+                            data: doc.data()
+                        })
+                    })
+                    this.setState({
+                        posts: postsAux
+                    })
+                }
+            )
         }
-
-    )}
     render(){
         return(
             <View style = {styles.container}>
+                <>
                 <Text> Home </Text>
+                <Text>Hola {auth.currentUser.email}!</Text>
+                </>
+                <TouchableOpacity style = {styles.button} onPress={() => this.props.handleLogout()}>
+                    <Text style = {styles.text}> Cerrar sesión </Text>
+                </TouchableOpacity>
+                {/*<Text>Ups! Todavía no hay posts disponibles</Text>*/}
                 <FlatList
                 data = {this.state.posts}
-                keyExtractor = {post => post.id.toString()} //pasamos el id a String
+                keyExtractor = {post => post.id.toString()}
                 renderItem = { ({item}) => 
                     <Post item = {item}></Post> }
                 />
