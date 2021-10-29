190e70677f6f55c21c1b7554c5de199c234e42c9
Josefina <jstaudenmaier@udesa.edu.ar>
2021-10-29 12:12:37 -0300

    cambio de handleLikes en Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,7 +7,7 @@ export default function Post ({item}){
 return(
     <View>
         <Text>{item.data.description}</Text>
-        <TouchableOpacity onPress={() => this.props.handleLikes(likes)}><Text><i class="far fa-heart"></i></Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => this.props.handleLikes()}><Text><i class="far fa-heart"></i></Text></TouchableOpacity>
         <Text>{item.data.createdAt}</Text>
         <Text>{item.data.owner}</Text>
     </View>
