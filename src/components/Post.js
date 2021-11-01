7d1d466c272a5969548da85743094dc836ec935e
Josefina <jstaudenmaier@udesa.edu.ar>
2021-11-01 10:34:56 -0300

    handle like y dislike

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,79 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { farHeart } from '@fortawesome/free-solid-svg-icons'
+import { fasHeart } from '@fortawesome/free-solid-svg-icons'
+import firebase from 'firebase';
 
-export default function Post ({item}){
-   
-return(
+export default class Post extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            liked: false,
+            likes:0,
+
+        }
+    }
+
+    componentDidMount(){
+        if(this.props.item.data.likes.length !== 0 ){
+            this.setState({
+                likes: this.props.item.data.likes.length
+            })
+        }
+
+    }
+
+    handleLikes(){
+        const actualizamosPost = db.collection('posts').doc(this.props.id)
+        actualizamosPost.update({
+            likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.displayName)
+
+        })
+        .then(()=>{
+            this.setState({
+                liked: true,
+                likes: this.state.likes +1
+            })
+
+        })
+        
+    }
+    handleDislikes(){
+        const actualizamosPost = db.collection('posts').doc(this.props.id)
+        actualizamosPost.update({
+            likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.displayName)
+
+        })
+        .then(()=>{
+            this.setState({
+                liked: false,
+                likes: this.state.likes -1
+            })
+
+        })
+        
+    }
+
+
+   render(){
+       return(
     <View styles={styles.container}>
-        <Text>{item.data.description}</Text>
-        <TouchableOpacity onPress={() => this.props.handleLikes()}><Text><FontAwesomeIcon icon={faHeart}/></Text></TouchableOpacity>
+        <Text>{item.data.description}</Text> {
+            ! this.state.liked ?
+            <TouchableOpacity onPress={() => this.props.handleLikes()}><Text><FontAwesomeIcon icon={farHeart}/></Text></TouchableOpacity>
+            :
+            <TouchableOpacity onPress={() => this.props.handleDislikes()}><Text><FontAwesomeIcon icon={fasHeart}/></Text></TouchableOpacity>
+ 
+
+        }
         <Text>{item.data.createdAt}</Text>
         <Text>{item.data.owner}</Text>
     </View>
-)
+)}
+
 }
 
 const styles = StyleSheet.create({
