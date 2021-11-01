090f4f05a7b3b363a9420f754b5435d2e1fd63d8
Nico Bilinkis <nico@bilinkis.com>
2021-11-01 09:00:50 -0300

    uso de font awesome en Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 export default function Post ({item}){
    
 return(
     <View styles={styles.container}>
         <Text>{item.data.description}</Text>
-        <TouchableOpacity onPress={() => this.props.handleLikes()}><Text><i class="far fa-heart"></i></Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => this.props.handleLikes()}><Text><FontAwesomeIcon icon={faHeart}/></Text></TouchableOpacity>
         <Text>{item.data.createdAt}</Text>
         <Text>{item.data.owner}</Text>
     </View>
